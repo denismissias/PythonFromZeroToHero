ed22ede948f268edec0b69535784551831fbd756
Denis Missias <denis.missias@gmail.com>
2018-07-05 23:30:28 -0300

    Adjust repository

---
+++ biblioteca.py
@@ -0,0 +1,13 @@
+def gera_nome_convite(convite):
+    posicao_final = len(convite)
+    posicao_inicial = posicao_final - 4
+    parte1 = convite[0:4]
+    parte2 = convite[posicao_inicial:posicao_final]
+    return "%s %s" % (parte1, parte2)
+
+def envia_convite(nome_formatado):
+    print "Enviando convite para %s" % (nome_formatado)
+
+def processa_convite(nome):
+    nome_formatado = gera_nome_convite(nome)
+    envia_convite(nome_formatado)
