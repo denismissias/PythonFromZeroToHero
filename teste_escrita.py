d5556aa053353c40a96de1ed9d67d6954da03800
Denis Missias <denis@socialminer.com>
2019-06-21 16:55:28 -0300

    Static method

--- teste_escrita.py
+++ teste_escrita.py
@@ -1,4 +1,6 @@
-arquivo = open('teste.txt', 'a')
-arquivo.write('Python rocks \n')
+arquivo = open('perfis.csv', 'a')
+arquivo.write('Denis, 1198766-5432, Empresa A \n')
+arquivo.write('Maria, 1198766-5123, Empresa B \n')
+arquivo.write('Jose, 1198766-5456, Empresa C \n')
 print arquivo.mode
 arquivo.close()
